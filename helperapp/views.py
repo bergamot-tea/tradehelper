4fd80e14f6ad63d96ab8bb0b6dc114ca782f2010
bergamot-tea <77209100+bergamot-tea@users.noreply.github.com>
2022-08-22 16:23:44 +0500

    from spirit to expert

--- helperapp/views.py
+++ helperapp/views.py
@@ -42,6 +42,9 @@ def check_delta_price (tokenpair):
     return(list_delta_price)
 
 
+def about_view(request):
+ 
+    return render(request, 'about.html', )  
 
 def donate_view(request):
  
@@ -90,7 +93,7 @@ def coin_view(request, coin):
     time_1d_early = timenow - timedelta(days = 1)
     time_30d_early = timenow - timedelta(days = 30)
     
-    spirit_list = ['fire','water', 'earth', 'air']
+    expert_list = ['fire','water', 'earth', 'air']
     try:
         token = Coins.objects.get(tiker=coin)
         tokenpair = token.pair
@@ -119,7 +122,8 @@ def coin_view(request, coin):
         xxxx_1d = [0] * сount_1d #будет список c датами-временем предиктов
         j8 = 0
         for i in predicts_fire_1d:
-            str_time = str(i.time_close)
+            time_msk = i.time_close + timedelta(hours=6)    #чтоб на странице отображалось не UTC(как в базе), а UTC+3 (Московское) 
+            str_time = str(time_msk)
             xxxx_1d[j8] = str_time[:-9]  # удаляем лишнюю точность времени(последние 9 символов строки) #в тимплейте в скрипте vue при передаче списка не забудь добавить safe, потому что javascript не понравятся ковычки - вот так например {{ xxxx|safe }}
             j8 = j8 + 1    
 
@@ -130,7 +134,8 @@ def coin_view(request, coin):
         xxxx_1h = [0] * сount_1h #будет список c датами-временем предиктов
         j9 = 0
         for i in predicts_fire_1h:
-            str_time = str(i.time_close)
+            time_msk = i.time_close + timedelta(hours=6)    #чтоб на странице отображалось не UTC(как в базе), а UTC+3 (Московское) 
+            str_time = str(time_msk)
             xxxx_1h[j9] = str_time[:-9]  # удаляем лишнюю точность времени(последние 9 символов строки) #в тимплейте в скрипте vue при передаче списка не забудь добавить safe, потому что javascript не понравятся ковычки - вот так например {{ xxxx|safe }}
             j9 = j9 + 1 
             
@@ -140,7 +145,8 @@ def coin_view(request, coin):
         xxxx_7d = [0] * сount_7d #будет список c датами-временем предиктов
         j10 = 0
         for i in predicts_fire_7d:
-            str_time = str(i.time_close)
+            time_msk = i.time_close + timedelta(hours=6)    #чтоб на странице отображалось не UTC(как в базе), а UTC+3 (Московское) 
+            str_time = str(time_msk)
             xxxx_7d[j10] = str_time[:-9]  # удаляем лишнюю точность времени(последние 9 символов строки) #в тимплейте в скрипте vue при передаче списка не забудь добавить safe, потому что javascript не понравятся ковычки - вот так например {{ xxxx|safe }}
             j10 = j10 + 1             
 
@@ -148,7 +154,8 @@ def coin_view(request, coin):
         j2 = 0
         zzzz = [0] * j #будет список с ценами
         for i in predicts_fire:
-            str_time = str(i.time_close)
+            time_msk = i.time_close + timedelta(hours=6)    #чтоб на странице отображалось не UTC(как в базе), а UTC+3 (Московское) 
+            str_time = str(time_msk)
             xxxx[j2] = str_time[:-9]  # удаляем лишнюю точность времени(последние 9 символов строки) #в тимплейте в скрипте vue при передаче списка не забудь добавить safe, потому что javascript не понравятся ковычки - вот так например {{ xxxx|safe }}
             zzzz[j2] = i.price
             j2 = j2 + 1
@@ -157,7 +164,7 @@ def coin_view(request, coin):
         #--------------график часовых предиктов за день------------------------
         j3 = 0
         yyyy = [0] * 8 #будет список из восьми длинных списков: 0 - прогнозы fire за неделю, 1 - труорфолсы fire за неделю (цвета зеленый красный серый для графика), 2 - пронозы water, 3 - труорфолс water и т.д.
-        for i in spirit_list:
+        for i in expert_list:
             my_filter = {}
             my_filter['period'] = '1h'
             my_filter['pair'] = tokenpair
@@ -196,7 +203,7 @@ def coin_view(request, coin):
  
         j5 = 0
         aaaa = [0] * 8 #будет список из восьми длинных списков: 0 - прогнозы fire за неделю, 1 - труорфолсы fire за неделю (цвета зеленый красный серый для графика), 2 - пронозы water, 3 - труорфолс water и т.д.
-        for i in spirit_list:
+        for i in expert_list:
             
             my_filter = {}
             my_filter['period'] = '1d'
@@ -239,7 +246,7 @@ def coin_view(request, coin):
  
         j10 = 0
         bbbb = [0] * 8 #будет список из восьми длинных списков: 0 - прогнозы fire , 1 - труорфолсы fire  (цвета зеленый красный серый для графика), 2 - пронозы water, 3 - труорфолс water и т.д.
-        for i in spirit_list:
+        for i in expert_list:
             
             my_filter = {}
             my_filter['period'] = '7d'
@@ -288,9 +295,27 @@ def coin_view(request, coin):
         return redirect ('/')    
     
 
-def spirit_view(request, spirit):
-    spirit_list = ['fire','water', 'earth', 'air']
-    if spirit in spirit_list:
+def expert_view(request, expert):
+    
+    
+#----этот кусок очень не хотелось, но пришлось написать, чтоб у экспертов были ссылки по их именам, так как из адресной строки браузера принимаем 'ilon','vitalik','satoshi','chanpen', а надо 'fire','water', 'earth', 'air'  
+    if expert not in ['ilon','vitalik','satoshi','chanpen']:
+        return redirect ('/')
+    
+    if expert in ['ilon','vitalik','satoshi','chanpen']:
+        if expert == 'ilon':
+            expert = 'fire'
+        elif expert == 'vitalik':
+            expert = 'water'
+        elif expert == 'satoshi':
+            expert = 'earth'
+        else:
+            expert = 'air'            
+#--------------------------------------------------------------------------    
+    
+   
+    expert_list = ['fire','water', 'earth', 'air']
+    if expert in expert_list:
 
     
         tokens = Coins.objects.all()
@@ -307,7 +332,7 @@ def spirit_view(request, spirit):
             for period in list_period:
                 my_filter = {}
                 my_filter['period'] = period
-                my_filter['nn__icontains'] = spirit
+                my_filter['nn__icontains'] = expert
                 my_filter['pair'] = tokenpair
                 try:
                     predict = Predict_grow.objects.filter(**my_filter).last().value
@@ -319,22 +344,22 @@ def spirit_view(request, spirit):
 
 
         #для графика с процентом правильности прогнозов по периодам
-        true_count_1h = Predict_grow.objects.filter(period = '1h').filter(trueorfalse = True).filter(nn__icontains = spirit).count()
-        false_count_1h = Predict_grow.objects.filter(period = '1h').filter(trueorfalse = False).filter(nn__icontains = spirit).count()
+        true_count_1h = Predict_grow.objects.filter(period = '1h').filter(trueorfalse = True).filter(nn__icontains = expert).count()
+        false_count_1h = Predict_grow.objects.filter(period = '1h').filter(trueorfalse = False).filter(nn__icontains = expert).count()
         try:
             percent_1h = true_count_1h / (true_count_1h + false_count_1h)
         except:
             percent_1h = 'Dontknow'  
         
-        true_count_1d = Predict_grow.objects.filter(period = '1d').filter(trueorfalse = True).filter(nn__icontains = spirit).count()
-        false_count_1d = Predict_grow.objects.filter(period = '1d').filter(trueorfalse = False).filter(nn__icontains = spirit).count()
+        true_count_1d = Predict_grow.objects.filter(period = '1d').filter(trueorfalse = True).filter(nn__icontains = expert).count()
+        false_count_1d = Predict_grow.objects.filter(period = '1d').filter(trueorfalse = False).filter(nn__icontains = expert).count()
         try:
             percent_1d = true_count_1d / (true_count_1d + false_count_1d)
         except:
             percent_1d = 'Dontknow'  
             
-        true_count_7d = Predict_grow.objects.filter(period = '7d').filter(trueorfalse = True).filter(nn__icontains = spirit).count()
-        false_count_7d = Predict_grow.objects.filter(period = '7d').filter(trueorfalse = False).filter(nn__icontains = spirit).count()
+        true_count_7d = Predict_grow.objects.filter(period = '7d').filter(trueorfalse = True).filter(nn__icontains = expert).count()
+        false_count_7d = Predict_grow.objects.filter(period = '7d').filter(trueorfalse = False).filter(nn__icontains = expert).count()
         try:
             percent_7d = true_count_7d / (true_count_7d + false_count_7d)
         except:
@@ -351,8 +376,8 @@ def spirit_view(request, spirit):
             list2[j3] = [i.name, i.tiker, i.logo]
             tokenpair = i.pair            
             
-            true_count_1h_token = Predict_grow.objects.filter(period = '1h').filter(trueorfalse = True).filter(nn__icontains = spirit).filter(pair = tokenpair).count()
-            false_count_1h_token = Predict_grow.objects.filter(period = '1h').filter(trueorfalse = False).filter(nn__icontains = spirit).filter(pair = tokenpair).count()
+            true_count_1h_token = Predict_grow.objects.filter(period = '1h').filter(trueorfalse = True).filter(nn__icontains = expert).filter(pair = tokenpair).count()
+            false_count_1h_token = Predict_grow.objects.filter(period = '1h').filter(trueorfalse = False).filter(nn__icontains = expert).filter(pair = tokenpair).count()
             try:
                 percent_1h_token = 100*true_count_1h_token / (true_count_1h_token + false_count_1h_token)
                 percent_1h_token = round(percent_1h_token,1)
@@ -360,8 +385,8 @@ def spirit_view(request, spirit):
                 percent_1h_token = 'Dontknow'  
             list2[j3].append(percent_1h_token)
             
-            true_count_1d_token = Predict_grow.objects.filter(period = '1d').filter(trueorfalse = True).filter(nn__icontains = spirit).filter(pair = tokenpair).count()
-            false_count_1d_token = Predict_grow.objects.filter(period = '1d').filter(trueorfalse = False).filter(nn__icontains = spirit).filter(pair = tokenpair).count()
+            true_count_1d_token = Predict_grow.objects.filter(period = '1d').filter(trueorfalse = True).filter(nn__icontains = expert).filter(pair = tokenpair).count()
+            false_count_1d_token = Predict_grow.objects.filter(period = '1d').filter(trueorfalse = False).filter(nn__icontains = expert).filter(pair = tokenpair).count()
             try:
                 percent_1d_token = 100*true_count_1d_token / (true_count_1d_token + false_count_1d_token)
                 percent_1d_token = round(percent_1d_token,1)
@@ -369,8 +394,8 @@ def spirit_view(request, spirit):
                 percent_1d_token = 'Dontknow'  
             list2[j3].append(percent_1d_token)
             
-            true_count_7d_token = Predict_grow.objects.filter(period = '7d').filter(trueorfalse = True).filter(nn__icontains = spirit).filter(pair = tokenpair).count()
-            false_count_7d_token = Predict_grow.objects.filter(period = '7d').filter(trueorfalse = False).filter(nn__icontains = spirit).filter(pair = tokenpair).count()
+            true_count_7d_token = Predict_grow.objects.filter(period = '7d').filter(trueorfalse = True).filter(nn__icontains = expert).filter(pair = tokenpair).count()
+            false_count_7d_token = Predict_grow.objects.filter(period = '7d').filter(trueorfalse = False).filter(nn__icontains = expert).filter(pair = tokenpair).count()
             try:
                 percent_7d_token = 100*true_count_7d_token / (true_count_7d_token + false_count_7d_token)
                 percent_7d_token = round(percent_7d_token,1)
@@ -381,7 +406,7 @@ def spirit_view(request, spirit):
         
         
     
-        return render(request, 'spirit.html', {'spirit': spirit,
+        return render(request, 'expert.html', {'expert': expert,
         'list1': list1,
 #        'true_count_1h': true_count_1h, 'false_count_1h': false_count_1h,
 #        'true_count_1d': true_count_1d, 'false_count_1d': false_count_1d,
--- tradehelper/views.py
+++ tradehelper/views.py
@@ -1,12 +1,16 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import login, authenticate, logout
 
-
+import random
+import os
 
 def home_view(request):
-
+    
+    folder = 'static/img/comics/'
+    files = os.listdir(folder)
+    comics = random.choice(files)
           
-    return render(request, 'index.html')
+    return render(request, 'index.html', {'comics': comics,})
 
 
 
